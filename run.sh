f2b09bd90110ed077ca34d0d7fb511a1aba2a5fe
Niruktha Roy Gotoor <gniruktharoy@gmail.com>
2017-07-10 16:31:48 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,4 @@
 
 # I'll execute my programs, with the input directory log_input and output the files in the directory log_output
 #python ./src/process_log.py ./log_input/log.txt ./log_output/hosts.txt ./log_output/hours.txt ./log_output/resources.txt ./log_output/blocked.txt
-python ./ip_addr.py > hosts.txt
-python ./bandwidth.py > hours.txt
-python ./timestamp.py > resources.txt
-python ./blocked.py > blocked.txt
+
